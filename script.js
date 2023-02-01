2c7a2af94344e3e07ea1685d28e39acc78996b22
Gage <yaujin@protonmail.com>
2023-01-31 20:05:01 -0600

    made object to hold array of all squares for board and made makeBoard function to create elements to dom

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const gameboardModule = (() => {})();
+const gameboardModule = (() => {
+  const gameboard = {
+    board: [],
+  };
+
+  function makeBoard(Array) {
+    const body = document.querySelector('body');
+
+    for (let i = 0; i < 9; i += 1) {
+      const square = body.appendChild(document.createElement('div'));
+      Array.push(square);
+    }
+  }
+
+  makeBoard(gameboard.board);
+})();
